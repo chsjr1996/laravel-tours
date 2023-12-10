6db20a9a003da1167e0f76932f5f117ac55cab23
Carlos Henrique <chsjr1996@gmail.com>
2023-12-10 17:23:42 -0300

    project/starter: Add Inertia.JS
    Install and prepare initial Inertia.JS configuration with Vue.JS.

---
+++ resources/js/pages/home.vue
@@ -0,0 +1,11 @@
+<template>
+    <p>Hello {{ text }}</p>
+</template>
+
+<script>
+export default {
+    props: {
+        text: String
+    }
+}
+</script>
--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,4 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', fn () => inertia('home', ['text' => 'world']));
